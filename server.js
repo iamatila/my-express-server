ff574e0327ccb91cb2e80db3182bce3f364533c5
tobiloba <tobiatilola@gmail.com>
2020-03-07 04:31:52 +0100

    my first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app =express();
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/contact', function (req, res) {
+  res.send('Contact me on tobiatilola@gmail.com');
+});
+
+app.get('/about', function (req, res) {
+  res.send('My name is Oluwatobiloba Atilola, i\'m a software developer, this is my test about pages');
+});
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
+});
